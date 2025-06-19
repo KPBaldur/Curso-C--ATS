754cabd2a4c2febeb58fced60710b58babce7131
BaldurDev <kevin.pizarrosanz@gmail.com>
2025-06-19 15:40:28 -0400

    Ejercicio Numero par

---
+++ Ejercicios de Practica/numeroPar.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Ingrese un numero: ";
+    cin >> a;
+
+    if (a % 2 == 0) {
+        cout << "El numero es par.";
+    } else {
+        cout << "El numero es impar.";
+    }
+
+    return 0;
+}
